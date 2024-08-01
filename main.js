25338ad096040ca83c90cb340546866698fb0050
infoMagicofart <info.magicofart@gmail.com>
2024-08-01 11:07:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+document.querySelector("#open-nav-menu").addEventListener("click", function(){
+    document.querySelector("header nav .wrapper").classList.add("nav-open");
+});
+document.querySelector("#close-nav-menu").addEventListener("click", function(){
+    document.querySelector("header nav .wrapper").classList.remove("nav-open");
+});
+
+
+
+//
+// const greetingText = "Good morning!";
+// const weatherCondition = "sunny";
+// const userLocation = "New Yourk";
+// let temperature = 22.8475;
+// let weatherText = "Today is a great day"; 
+
+
